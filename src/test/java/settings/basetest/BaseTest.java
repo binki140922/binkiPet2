eb9e1758fb862bfc4b29742fa56cb1ab98727732
v.pakhomia <v.pakhomia@banki.ru>
2023-02-20 22:02:24 +0300

    home work

--- src/test/java/settings/basetest/BaseTest.java
+++ src/test/java/settings/basetest/BaseTest.java
@@ -3,6 +3,7 @@ package settings.basetest;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import settings.helper.Attach;
 import settings.pages.RegistrationForm;
 
@@ -16,6 +17,13 @@ public abstract class BaseTest {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
