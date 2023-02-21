2ac255ff8a35d740354d5db63822e86ea03dda08
v.pakhomia <v.pakhomia@banki.ru>
2023-02-21 22:37:03 +0300

    home work

--- src/test/java/zipTest/ZipTest.java
+++ src/test/java/zipTest/ZipTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ZipTest {
 
     @Test
-    public void ZipExtractTest() throws IOException, CsvException {
+    public void zipExtractTest() throws IOException, CsvException {
 
         ClassLoader classLoader = ZipTest.class.getClassLoader();
 
