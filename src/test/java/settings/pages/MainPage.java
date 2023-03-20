b789c5d43327d60a69ac2c0870785dfe28a28d0b
v.pakhomia <v.pakhomia@banki.ru>
2023-03-20 22:35:29 +0300

    add tests

--- src/test/java/settings/pages/MainPage.java
+++ src/test/java/settings/pages/MainPage.java
@@ -1,8 +1,12 @@
 package settings.pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
+import lombok.Getter;
+import lombok.Setter;
 import settings.baseTest.BaseTest;
 import settings.configs.TestConfig;
 import settings.pages.components.HeaderDesktopLayout;
@@ -12,22 +16,57 @@ public class MainPage extends BaseTest {
 
     HeaderDesktopLayout headerDesktopLayout = new HeaderDesktopLayout();
 
+    private SelenideElement tmpSelenideElement;
+
     @Step("Открываем главную страницу")
-    public MainPage openMainPage(){
+    public MainPage openMainPage() {
         TestUtil.openPage(TestConfig.getBaseUrl());
         return this;
     }
 
     @Step("Нажимаем на хедер - {}")
-    public MainPage clickHeader(String headerName){
+    public MainPage clickHeader(String headerName) {
         headerDesktopLayout.clickHeader(headerName);
         return this;
     }
 
-    @Step("Проверяем верность переадресации")
-    public MainPage checkOpenUrl(String fullUrl){
-        TestUtil.checkUrlPage(TestConfig.getBaseUrl() + fullUrl);
+    @Step("Нажимаем на элемент - {}")
+    public MainPage findAndClickText(String nameElement) {
+        Selenide.$(Selectors.byText(nameElement)).click();
         return this;
     }
 
+    @Step("Выбрать елемент у которого атрибут равен - {}")
+    public MainPage findAttributElement(String nameAttribute, String valueAttribute) {
+        tmpSelenideElement = Selenide.$(Selectors.byAttribute(nameAttribute, valueAttribute));
+        return this;
+    }
+
+    @Step("Выбрать елемент у которого атрибут равен - {} и нажать")
+    public MainPage findAndClickAttributElement(String nameAttribute, String valueAttribute) {
+        Selenide.$(Selectors.byAttribute(nameAttribute, valueAttribute)).click();
+        return this;
+    }
+
+    @Step("Ввести и активировать - {}")
+    public MainPage setText(String text) {
+        Selenide.$(tmpSelenideElement).setValue(text).pressEnter();
+        return this;
+    }
+
+    @Step("Проверяем верность переадресации")
+    public void checkOpenUrl(String fullUrl) {
+        TestUtil.checkUrlPage(TestConfig.getBaseUrl() + fullUrl);
+    }
+
+    @Step("Проверяем видимость элемента")
+    public void checkVisibleElement(String nameElement) {
+        Selenide.$(Selectors.byText(nameElement)).shouldBe(Condition.visible);
+    }
+
+    @Step("Проверяем наличие текста {} в первом результате поиска")
+    public void checkOneResultFaind(String text) {
+        Selenide.$$(".b-serp-item__content").get(0).$(".b-serp-item__title-link")
+                .shouldBe(Condition.text(text));
+    }
 }
--- src/test/java/tests/web/MainPageDesktopTest.java
+++ src/test/java/tests/web/MainPageDesktopTest.java
@@ -1,86 +1,71 @@
 package tests.web;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
-import io.qameta.allure.Allure;
-import org.junit.jupiter.api.*;
-
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import settings.baseTest.BaseTest;
 import settings.data.HeaderDLData;
 import settings.pages.MainPage;
 
-import static org.assertj.core.api.ClassBasedNavigableIterableAssert.assertThat;
-
 @Tag("desktop")
 public class MainPageDesktopTest extends BaseTest {
 
     MainPage mainPage = new MainPage();
 
-    @Disabled
-    @Tags(value = {@Tag("regress"),  @Tag("sanity")})
+    @Tags(value = {@Tag("regress"), @Tag("sanity")})
     @DisplayName("Проверка хедера - {}")
     @EnumSource(value = HeaderDLData.class, mode = EnumSource.Mode.INCLUDE)
     @ParameterizedTest
-    public void checkHeaderDesktopLayout(HeaderDLData data){
+    public void checkHeaderDesktopLayout(HeaderDLData data) {
 
         mainPage.openMainPage()
                 .clickHeader(data.getLinkName())
                 .checkOpenUrl(data.getLinkUrl());
     }
 
-    @Disabled
-    @Tags(value = {@Tag("regress"),  @Tag("sanity")})
+    @Tag("regress")
     @DisplayName("Проверка хедера - {}")
     @Test
-    public void checkButtonAlfaOnline(){
+    public void checkButtonAlfaOnline() {
 
-        mainPage.openMainPage();
-        Selenide.$(Selectors.byText("Альфа-Онлайн")).click();
-
-        Selenide.$(Selectors.byText("войти в Альфа-Онлайн?")).shouldBe(Condition.visible);
+        mainPage.openMainPage()
+                .findAndClickText("Альфа-Онлайн")
+                .checkVisibleElement("войти в Альфа-Онлайн?");
     }
 
-    @Disabled
-    @Tags(value = {@Tag("regress"),  @Tag("sanity")})
+    @Tag("regress")
     @DisplayName("Проверка хедера - {}")
     @Test
-    public void mortgageLayoutTest(){
+    public void mortgageLayoutTest() {
 
-        mainPage.openMainPage();
-        Selenide.$(Selectors.byText("СТАТЬ КЛИЕНТОМ")).click();
-        Selenide.$(Selectors.byAttribute("data-test-id","layout")).$(Selectors.byText("Ипотека")).click();
-
-        Selenide.$(Selectors.byText("Узнайте лимит по ипотеке")).shouldBe(Condition.visible);
+        mainPage.openMainPage()
+                .findAndClickText("СТАТЬ КЛИЕНТОМ")
+                .findAttributElement("data-test-id", "layout")
+                .findAndClickText("Ипотека")
+                .checkVisibleElement("Узнайте лимит по ипотеке");
     }
 
-    @Disabled
-    @Tags(value = {@Tag("regress"),  @Tag("sanity")})
+    @Tag("regress")
     @DisplayName("Проверка хедера - {}")
     @Test
-    public void mortgageFaindTest(){
+    public void mortgageFaindTest() {
 
-        mainPage.openMainPage();
-        Selenide.$(Selectors.byAttribute("placeholder","Я ищу")).setValue("ипотеке").pressEnter();
-        Selenide.$$(".b-serp-item__content").get(0).$(".b-serp-item__title-link")
-                .shouldBe(Condition.text(" — Оформить заявку онлайн на ипотечный кредит..."));
-    }
+        mainPage.openMainPage()
+                .findAttributElement("placeholder", "Я ищу")
+        .setText("ипотека")
+                .checkOneResultFaind(" — Оформить заявку онлайн на ипотечный кредит...");
+}
 
-
-    @Tags(value = {@Tag("regress"),  @Tag("sanity")})
+    @Tags(value = {@Tag("regress"), @Tag("smoke")})
     @DisplayName("Проверка хедера - {}")
     @Test
-    public void calculatedTest(){
+    public void calculatedTest() {
 
-        mainPage.openMainPage();
-        Selenide.$("span[]").click();
-        Selenide.$(Selectors.byAttribute("data-test-id","program-select")).click();
-        Selenide.$$(Selectors.byAttribute("data-test-id","autocomplete")).get(2).click();
-        Selenide.$("#creditTerm").setValue("30");
-        Selenide.$(Selectors.byAttribute("data-test-id","monthly-payment"))
-                .shouldBe(Condition.text("19 873 ₽"));
-
+        mainPage.openMainPage()
+                .findAndClickAttributElement("data-test-id", "Main-Header-Main-DesktopLogo")
+                .checkOpenUrl("");
     }
 }
